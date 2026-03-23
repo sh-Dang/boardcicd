fd83cbbcbe3f0835196dfaa974201890d4943a87
sh-Dang <henrys200632@gmail.com>
2026-03-24 00:49:11 +0900

    memberController에 TODO 주석 수정

--- backend/src/main/java/com/boardcicd/member/controller/MemberController.java
+++ backend/src/main/java/com/boardcicd/member/controller/MemberController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 로그인, 회원가입등 유저에 관련된 요청을 처리하기 위한 컨트롤러
+ *
+ * @author 이세형
+ * @since 2026/03/24
+ * */
 @RestController
 @RequestMapping("/api/auth")
 public class MemberController {
@@ -17,7 +23,7 @@ public class MemberController {
         System.out.println("가져온 유저네임: " + request.getUsername());
         System.out.println("가져온 비밀번호: " + request.getPassword());
 
-        // TODO: 로그인 검증 로직
+        // TODO: 로그인 검증 로직 추가 후 양식에 맞는 reponse 객체 반환
 
         return "ok";
     }
